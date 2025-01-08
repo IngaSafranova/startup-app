79c98160ae95aaf1438661a8a8662744547efb74
Inga Safranova <sfrnv@yahoo.co.uk>
2025-01-08 19:59:33 +0000

    login with github working

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
-import { auth , signIn, signOut} from '@/auth'
+import { auth , signOut, signIn} from '@/auth'
+import { redirect } from 'next/dist/server/api-utils'
 
 
 const Navbar = async() => {
-  const session = await auth
+  const session = await auth()
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
@@ -22,23 +23,24 @@ const Navbar = async() => {
               <Link href='/startup/create' >
                 <span>Create</span>
               </Link>
-              <button onClick={signOut} >
-                <span>Logout</span>
-              </button>
+              <form action={async () => {
+                'use server';
+                await signOut();
+              }} >
+                <button type='submit'>Logout</button>
+              </form>
               {/* Link to users page    */}
-              <Link href={`/users/${session?.id}`}>
+              <Link href={`/user/${session?.id}`}>
                 <span>{ session?.user?.name}</span>
               </Link>
             </>
-          ) : (
-              
-            <button onClick={async () => {
+          ): (
+              <form action={async () => {
                 'use server';
 
-              await signIn(provider:'github');
-              }}>
-           <span>Login</span>
-              </button>
+              await signIn()}}>
+           <button type='submit'>Login</button>
+              </form>
           )}
 
         </div>
--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
 // https://authjs.dev/getting-started/providers/github
-
 import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+
+
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Github],
+  providers: [GitHub],
 });
