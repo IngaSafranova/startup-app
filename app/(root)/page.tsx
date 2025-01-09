23078f733c70a3c80c98ec4662563249c03c4c92
Inga Safranova <sfrnv@yahoo.co.uk>
2025-01-09 20:41:18 +0000

    made utility classes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <>
       {/* created your own utility classes in globals.css */}
+      <section className="pink_container" >
+
+      </section >
       <h1 className="heading">Home</h1>
+      <h2  >
+
+      </h2>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -45,10 +45,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"),
-    require('tailwindcss-animate')
-
-  ],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 
 export default config;
