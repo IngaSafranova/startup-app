1ddc711aca3232fcf95464c8befa391ae473c4d7
Inga Safranova <sfrnv@yahoo.co.uk>
2025-01-27 13:20:55 +0000

    changes to tailwind

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -29,7 +29,7 @@ export default async function Home({
         <h1 className="heading">
           Pitch your Startup, <br /> Connect With Entrepreneurs
         </h1>
-        <p className="sub-heading !max-w-3xl">
+        <p className="sub-heading max-w-3xl!">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -28,10 +28,10 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   
   return (
     <>
-      <section className="pink_container !min-h-[230px]">
+      <section className="pink_container min-h-[230px]!">
         <p className="tag">{formateDate(post?._createdAt)} </p>
         <h1 className="heading"> {post.title}</h1>
-        <p className='sub-heading !max-w-5xl' >{post.description} </p>
+        <p className='sub-heading max-w-5xl!' >{post.description} </p>
       </section>
       <section className='section_container' >
         <img src={post.image} alt="tumbnail" className='w-full h-auto rounded-xl' />
@@ -47,7 +47,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
               />
               <div>
                 <p className='text-20-medium' >{post.author.name}</p>
-                <p className='text-16-medium !text-black-300' >@{post.author.username} </p>
+                <p className='text-16-medium text-black-300!' >@{post.author.username} </p>
               </div>
             </Link>
             <p className='category-tag' >{post.category} </p>
